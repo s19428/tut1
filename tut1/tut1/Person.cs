dc0a18cb4d8b68a132cfe8d3659016bc6bb8a5a6
s19428 <s19428@LAB132-07>
2020-03-06 15:35:08 +0100

    First commit

---
+++ tut1/tut1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tut1
+{
+    class Person
+    {
+        // ctrl + k ... ctrl + c
+        // ctrl + k ... ctrl + u
+        // cw ... tab twice
+
+        private string firstname;
+
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        public string LastName { get; private set; }
+
+        public void Print()
+        {
+            int? age = 25;
+            Console.WriteLine($"I Am {age} years old.");
+        }
+    }
+}
---
+++ tut1/tut1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tut1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            int? i = null;
+            Console.WriteLine("Hello World!");
+
+            Person p = new Person();
+            p.Print();
+
+            var url = @"https://www.pja.edu.pl/en/";
+            var httpClient = new HttpClient();
+            using (var response = await httpClient.GetAsync(url))
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(content);
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match + "");
+                }
+            }
+            //response.Dispose();
+        }
+    }
+}
